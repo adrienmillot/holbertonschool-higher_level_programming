ef0807eb4aca493934d920f92bf19a80141786b8
Adrien MILLOT <adrien.millot@gmail.com>
2021-05-21 23:07:08 +0200

    Add space managment

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -13,11 +13,17 @@ def text_indentation(prmText):
     if not isinstance(prmText, str):
         raise TypeError("text must be a string")
 
-    prmText = prmText.replace(". ", ".\n")
-    prmText = prmText.replace("? ", "?\n")
-    prmText = prmText.replace(": ", ":\n")
+    prmText = prmText.strip()
+    prmText = prmText.replace(".", ".\n")
+    prmText = prmText.replace("?", "?\n")
+    prmText = prmText.replace(":", ":\n")
+    prmText = prmText.rstrip()
+
+    if len(prmText) == 0:
+        return
 
     for key, line in enumerate(list(prmText.split("\n"))):
+        line = line.strip()
         print("{}".format(line))
         if key < len(list(prmText.split("\n"))) - 1:
             print()
