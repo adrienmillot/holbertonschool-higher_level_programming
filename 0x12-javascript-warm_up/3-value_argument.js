0ad072792fcf0a98a72aa8583414caddbde3c283
Adrien MILLOT <adrien.millot@gmail.com>
2021-08-02 12:15:27 +0200

    Fix length

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -4,8 +4,8 @@
  * prints the first argument passed to it
  */
 
-if (process.argv.length === 2) {
+if (process.argv[2] === undefined) {
   console.log('No argument');
-} else if (process.argv.length > 2) {
+} else {
   console.log(process.argv[2]);
 }
