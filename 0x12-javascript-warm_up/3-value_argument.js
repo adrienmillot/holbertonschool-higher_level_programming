75f214d669d1647d7ca78e79be03ab28dd620660
Adrien MILLOT <adrien.millot@gmail.com>
2021-08-02 11:02:01 +0200

    Value of my argument

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+/**
+ * prints the first argument passed to it
+ */
+
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else if (process.argv.length > 2) {
+  console.log(process.argv[2]);
+}
