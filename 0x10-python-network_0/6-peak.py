db4a8cae87f69fc3d6e8df2ea4aa11ec0c15e469
Adrien MILLOT <adrien.millot@gmail.com>
2021-08-19 19:54:42 +0200

    Fix efficiency

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,19 +6,7 @@ def find_peak(list_of_integers):
     """
         function that find the first peak
     """
-    if None is list_of_integers or len(list_of_integers) == 0:
+    if len(list_of_integers) <= 0:
         return None
-    if len(list_of_integers) == 1:
-        return list_of_integers[0]
-    for i in range(len(list_of_integers)):
-        if (
-            i < len(list_of_integers) - 1
-            and list_of_integers[i] > list_of_integers[i + 1]
-            and (
-                list_of_integers[i - 1] is None
-                or list_of_integers[i - 1] < list_of_integers[i]
-            )
-        ):
-            return list_of_integers[i]
-
-    return list_of_integers[i]
+    list_of_integers.sort()
+    return list_of_integers[-1]
